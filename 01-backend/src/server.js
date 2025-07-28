d3f2602a4ed34b7d15b795d851186de4f0e8448a
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-07-28 21:21:31 +0700

    delete and get favarites

--- 01-backend/src/server.js
+++ 01-backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./database/schema.js";
+import { and, eq } from "drizzle-orm";
 
 const app = express();
 const PORT = ENV.PORT;
@@ -34,6 +35,37 @@ app.post("/api/favorites", async (req, res) => {
         res.status(201).json(newFavorite[0]);
     } catch (error) {
         console.log("Error add favorites", error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+})
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+    try {
+        const { userId, recipeId } = req.params;
+
+        await db.delete(favoritesTable).where(
+            and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, recipeId, parseInt(recipeId)))
+        )
+        res.status(200).json({ message: "Favorites remove successfully" });
+
+    } catch (error) {
+        console.log("Error removing a favorites", error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+})
+
+app.get("/api/favorites/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        const userFavorites = await db
+            .select()
+            .from(favoritesTable)
+            .where(eq(favoritesTable.userId, userId));
+        res.status(200).json(userFavorites);
+
+    } catch (error) {
+        console.log("Error getting a favorites", error);
         res.status(500).json({ error: "Something went wrong" });
     }
 })
